ea443a97f9ca649755238a977107d8f522c3bc98
Santiago <fsgreco@fastmail.com>
2023-06-23 22:32:05 +0200

    Exercise 5.1 - keyboard users Pressing Escape remove all toasts

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -8,6 +8,13 @@ function ToastProvider({children}) {
 	const [ toasts, setToasts ] = React.useState([])
 	const addToast = toast => setToasts(currArr => [...currArr, toast ])
 	const removeToast = toastId => setToasts( currentArr => currentArr.filter( val => val.id !== toastId))
+
+	React.useEffect(() => {
+		const removeAllToasts = e => e.code === 'Escape' && setToasts([])
+		window.addEventListener('keydown', removeAllToasts )
+		return () => window.removeEventListener('keydown', removeAllToasts )
+	},[])
+
   return (
 		<CtxToasts.Provider value={{ toasts, addToast, removeToast }}> 
 			{children}

ff67b243e5fa49fdc458d7462c1f29849d6ec59e
Santiago <fsgreco@fastmail.com>
2023-06-23 19:47:02 +0200

    Exercise 2 - live-editable toast preview

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,22 +3,31 @@ import React from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast'
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
 	const [ message, setMessage ] = React.useState('')
 	const [ choosenVariant, setChoosenVariant ] = React.useState(VARIANT_OPTIONS[0])
+
+	const [ showToast, setShowToast ] = React.useState(false)
+
 	const handleSubmit = e => {
 		e.preventDefault()
 		console.log({message, choosenVariant})
+		setShowToast(true)
 	}
+	const handleDismiss = () => setShowToast(false)
+
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+			{ showToast && <Toast type={choosenVariant} content={message} handleDismiss={handleDismiss} /> }
 
       <form onSubmit={handleSubmit} className={styles.controlsWrapper}>
         <div className={styles.row}>
